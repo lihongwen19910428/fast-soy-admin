7bae895c00ed1fafbf43f9551d7020729d4347bd
sleep1223 <sleep1223@outlook.com>
2026-03-27 17:39:05 +0800

    feat(logs): update log search endpoint and adjust parameters

--- app/api/v1/system_manage/logs.py
+++ app/api/v1/system_manage/logs.py
@@ -13,20 +13,18 @@ from app.schemas.base import Fail, Success, SuccessExtra
 router = APIRouter()
 
 
-@router.post("/logs/all/", summary="查看日志列表")
+@router.post("/logs/search", summary="搜索日志列表")
 async def _(log_in: LogSearch):
-    if log_in.log_type is None:
-        log_in.log_type = LogType.ApiLog
+    log_type = log_in.log_type
+    if log_type is None:
+        log_type = LogType.ApiLog
 
-    if log_in.current is None:
-        log_in.current = 1
-
-    if log_in.size is None:
-        log_in.size = 10
+    current = log_in.current or 1
+    size = log_in.size or 10
 
     q = Q()
-    if log_in.log_type:
-        q &= Q(log_type=log_in.log_type)
+    if log_type:
+        q &= Q(log_type=log_type)
     if log_in.by_user:
         if (_by_user := await User.get_or_none(id=log_in.by_user)) is not None:
             q &= Q(by_user=_by_user)
@@ -51,39 +49,39 @@ async def _(log_in: LogSearch):
     user_role_objs: list[Role] = await user_obj.by_user_roles
     user_role_codes = [role_obj.role_code for role_obj in user_role_objs]
 
-    if "R_ADMIN" in user_role_codes and log_in.log_type not in [LogType.ApiLog, LogType.UserLog]:  # 管理员只能查看API日志和用户日志
+    if "R_ADMIN" in user_role_codes and log_type not in [LogType.ApiLog, LogType.UserLog]:  # 管理员只能查看API日志和用户日志
         return Fail(msg="Permission Denied")
-    elif "R_SUPER" not in user_role_codes and "R_ADMIN" not in user_role_codes and log_in.log_type != LogType.ApiLog:  # 非超级管理员和管理员只能查看API日志
+    elif "R_SUPER" not in user_role_codes and "R_ADMIN" not in user_role_codes and log_type != LogType.ApiLog:  # 非超级管理员和管理员只能查看API日志
         return Fail(msg="Permission Denied")
 
-    total, log_objs = await log_controller.list(page=log_in.current, page_size=log_in.size, search=q, order=["-id"])
+    total, log_objs = await log_controller.list(page=current, page_size=size, search=q, order=["-id"])
     records = []
     for obj in log_objs:
         await obj.fetch_related("api_log", "by_user")
         api_log: APILog = obj.api_log
-        by_user: User = obj.by_user
+        by_user_obj: User = obj.by_user
         record = await obj.to_dict(exclude_fields=["by_user_id", "api_log_id"])
-        if log_in.log_type == LogType.ApiLog:
+        if log_type == LogType.ApiLog:
             api_log_dict = await api_log.to_dict(exclude_fields=[])
             api_log_dict["requestParams"] = json.dumps(api_log_dict["requestParams"], ensure_ascii=False)
             api_log_dict["responseData"] = json.dumps(api_log_dict["responseData"], ensure_ascii=False)
             record.update(api_log_dict)
             record = {"logUser": "Request", **record}
-        elif log_in.log_type == LogType.SystemLog:
+        elif log_type == LogType.SystemLog:
             record["logUser"] = "System"
         else:
-            if by_user:
-                record["byUser"] = str(by_user.id)
-                record["byUserInfo"] = await by_user.to_dict(include_fields=["id", "nick_name"])
+            if by_user_obj:
+                record["byUser"] = str(by_user_obj.id)
+                record["byUserInfo"] = await by_user_obj.to_dict(include_fields=["id", "nick_name"])
             else:
                 record["byUser"] = None
 
         records.append(record)
     data = {"records": records}
-    return SuccessExtra(data=data, total=total, current=log_in.current, size=log_in.size)
+    return SuccessExtra(data=data, total=total, current=current, size=size)
 
 
-@router.get("/logs/{log_id}", summary="查看日志")
+@router.get("/logs/{log_id}", summary="查看日志详情")
 async def _(log_id: int):
     log_obj = await log_controller.get(id=log_id)
     data = await log_obj.to_dict(exclude_fields=["id", "create_time", "update_time"])
