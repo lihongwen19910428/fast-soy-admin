53efe74dee0db07d0bc33233e7bdcd2e6806e494
sleep1223 <sleep1223@outlook.com>
2026-03-28 14:28:39 +0800

    feat(init_app): enhance PrettyErrorsMiddleware configuration for better error visibility

--- app/core/init_app.py
+++ app/core/init_app.py
@@ -1,3 +1,4 @@
+import pretty_errors
 from aerich import Command
 from fastapi import FastAPI
 from fastapi.middleware import Middleware
@@ -27,7 +28,17 @@ from app.settings import APP_SETTINGS
 
 def make_middlewares():
     middleware = [
-        Middleware(PrettyErrorsMiddleware),
+        Middleware(
+            PrettyErrorsMiddleware,
+            line_number_first=True,
+            lines_before=5,
+            lines_after=2,
+            line_color=pretty_errors.RED + "> " + pretty_errors.default_config.line_color,
+            code_color="  " + pretty_errors.default_config.line_color,
+            truncate_code=True,
+            display_locals=True,
+            filename_display=pretty_errors.FILENAME_EXTENDED,
+        ),
         Middleware(
             CORSMiddleware,
             allow_origins=APP_SETTINGS.CORS_ORIGINS,
